3c5f7caa9d57f343d3282e21fad962c3c9e32752
Ihor Oryshchenko <igor89.or@gmail.com>
2022-07-10 02:42:25 +0300

    added backend surveys logic

---
+++ services/Mailer.js
@@ -0,0 +1,24 @@
+const sgMail = require('@sendgrid/mail');
+const keys = require('../config/keys');
+
+class Mailer {
+    constructor({ subject, recipients }, content) {
+        sgMail.setApiKey(keys.sendGridKey);
+        console.log(recipients);
+        this.msg = {
+            to: recipients.map(({ email }) => email),
+            from: 'igor89.or@gmail.com',
+            subject: subject,
+            html: content,
+            trackingSettings: { enable_text: true, enabled: true },
+
+        };
+    }
+
+    async send() {
+        const response = await sgMail.send(this.msg);
+        return response;
+    }
+}
+
+module.exports = Mailer;
