7cc0b346f1c34262b4ad2ad067af7db3ab1dd012
Ihor Oryshchenko <igor89.or@gmail.com>
2022-07-17 14:23:16 +0300

    fixing redundant code

--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,6 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-//TODO delete(test) ************************************
-import axios from 'axios';
-window.axios = axios;
-// const survey = {title: 'My Title', subject: 'subject', recipients: 'ua.electrolux.official@gmail.com, ua.zanussi.official@gmail.com', body: 'heres the body of email'}
-// axios.post('/api/surveys', survey)
-//******************************************
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 const container = document.getElementById('root');
